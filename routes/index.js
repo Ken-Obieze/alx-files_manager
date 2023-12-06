451b96f2eaff86172d8dae1a7051f46409be40d2
Ken-Obieze <kenneth.obieze@outlook.com>
2023-12-06 15:44:48 +0100

    file manager task

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
+const FilesController = require('../controllers/FilesController');
+
+
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
+export default router;
